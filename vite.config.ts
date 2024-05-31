9e470c9366d4cb52d889e94f0df9040d90cd4aeb
Ryan Lelek <ryanlelek@gmail.com>
2024-05-31 16:24:45 -0500

    Edit: Vite config. Port 3000 and use cache-busting assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import pkgjson from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  publicDir: "static",
+  build: {
+    chunkSizeWarningLimit: 1000,
+    sourcemap: false,
+    assetsInlineLimit: 0,
+    outDir: "public",
+    assetsDir: `v${pkgjson.version}`,
+  },
 });
