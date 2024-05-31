65bbc187407ec59f2f376747e220f6e9c5a0a4a1
Ryan Lelek <ryanlelek@gmail.com>
2024-05-31 17:06:49 -0500

    Add: Favicon, logo, and webmanifest

---
+++ docs/LOGO.md
@@ -0,0 +1,18 @@
+# Logo
+
+You'll probably want to be using an SVG image file.  
+For simple or temporary designs, you can use an emoji.
+
+## Emoji Libraries:
+
+- [SVGmoji GitHub](https://github.com/svgmoji/svgmoji)
+- [Openmoji](https://openmoji.org)
+- [Twitter Emoji - Twemoji](https://github.com/twitter/twemoji)
+
+## Finding Emoji Character Codes
+
+Many emoji libraries use unicode character codes for their SVG file names.  
+You can find the character code using these tools.
+
+- [Emoji Guide](https://emojiguide.org/)
+- [Array of Sunshine](https://gadgets.arayofsunshine.dev/unicode)
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,27 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import vitePluginFaviconsInject from "vite-plugin-favicons-inject";
 import pkgjson from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // Based on this package (use to view options)
+    // https://github.com/itgalaxy/favicons
+    vitePluginFaviconsInject("./src/assets/vite.svg", {
+      appName: "ExampleName",
+      appShortName: "ExampleShort",
+      appDescription: "ExampleDescription",
+      developerName: "ExampleDeveloper",
+      developerURL: "https://example.com",
+      lang: "en-US",
+      background: "#333333",
+      appleStatusBarStyle: "default",
+      display: "standalone",
+      theme_color: "#444444",
+    }),
+  ],
   server: {
     port: 3000,
   },
