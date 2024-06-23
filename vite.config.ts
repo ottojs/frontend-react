d0a7a6a08e556cc78fb25041989238d87ae7366f
Ryan Lelek <ryanlelek@gmail.com>
2024-06-22 21:08:13 -0500

    Add: CSP connect-src for profile images https://storage.googleapis.com

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
       "X-Content-Type-Options": "nosniff",
       "X-Frame-Options": "DENY",
       "Content-Security-Policy":
-        "default-src 'none'; script-src 'self' 'unsafe-inline'; style-src 'unsafe-inline'; img-src 'self' https: data:; connect-src 'self' http://localhost:8080; font-src 'self'; object-src 'none'; media-src 'none'; frame-src 'none'; report-uri http://localhost:8080/csp-report; form-action 'self'; frame-ancestors 'none'; base-uri 'self'; manifest-src 'self'; script-src-attr 'none'; upgrade-insecure-requests",
+        "default-src 'none'; script-src 'self' 'unsafe-inline'; style-src 'unsafe-inline'; img-src 'self' https: data:; connect-src 'self' http://localhost:8080 https://storage.googleapis.com; font-src 'self'; object-src 'none'; media-src 'none'; frame-src 'none'; report-uri http://localhost:8080/csp-report; form-action 'self'; frame-ancestors 'none'; base-uri 'self'; manifest-src 'self'; script-src-attr 'none'; upgrade-insecure-requests",
     },
   },
   publicDir: "static",
