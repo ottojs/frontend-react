b419b291ca234ea124d157b58e67e0d9a8acdba9
Ryan Lelek <ryanlelek@gmail.com>
2024-06-27 05:39:10 -0500

    Add: Voice Recorder (Local)

--- src/pages/PageRecorder.tsx
+++ src/pages/PageRecorder.tsx
@@ -1,4 +1,28 @@
+// Modules
+import { useEffect, useState } from "react";
+import { useAudioRecorder } from "@fixhq/react-audio-voice-recorder";
+import Button from "react-bootstrap/Button";
+
 const PageRecorder = () => {
+  const [audioBlob, setAudioBlob] = useState<Blob>();
+  const {
+    startRecording,
+    stopRecording,
+    //togglePauseResume,
+    recordingBlob,
+    isRecording,
+    //isPaused,
+    //recordingTime,
+    //mediaRecorder,
+  } = useAudioRecorder();
+
+  useEffect(() => {
+    if (!recordingBlob) return;
+    console.log("RECBLOB");
+    setAudioBlob(recordingBlob);
+    // recordingBlob will be present at this point after 'stopRecording' has been called
+  }, [recordingBlob]);
+
   return (
     <div className="container">
       <div className="row">
@@ -6,6 +30,38 @@ const PageRecorder = () => {
           <h1>
             <i className="bi bi-mic"></i> Audio Recorder
           </h1>
+          <div className="m-3">
+            <Button
+              variant="danger"
+              onMouseDown={startRecording}
+              onMouseUp={stopRecording}
+              onTouchStart={startRecording}
+              onTouchEnd={stopRecording}
+              onTouchCancel={stopRecording}
+              onPointerCancel={stopRecording}
+            >
+              <i className="bi bi-mic"></i> Press and Hold to Record
+            </Button>
+          </div>
+          <div className="m-3">
+            {isRecording ? (
+              <Button variant="warning" onClick={stopRecording}>
+                Stop Button
+              </Button>
+            ) : (
+              <Button variant="danger" onClick={startRecording}>
+                Start Button
+              </Button>
+            )}
+          </div>
+          <div className="m-3">
+            {!isRecording && audioBlob && (
+              <audio
+                src={URL.createObjectURL(audioBlob)}
+                controls={true}
+              ></audio>
+            )}
+          </div>
         </div>
       </div>
     </div>
--- vite.config.ts
+++ vite.config.ts
@@ -22,13 +22,20 @@ export default defineConfig({
       theme_color: "#444444",
     }),
   ],
+  //base: "https://127.0.0.1:3000/",
   server: {
+    // host: true,
     port: 3000,
+    // strictPort: true,
+    // https: false,
+    // open: false,
+    // cors: false,
+    // origin: "http://127.0.0.1:3000",
     headers: {
       "X-Content-Type-Options": "nosniff",
       "X-Frame-Options": "DENY",
       "Content-Security-Policy":
-        "default-src 'none'; script-src 'self' 'unsafe-inline'; style-src 'unsafe-inline'; img-src 'self' https: data:; connect-src 'self' http://localhost:8080 https://storage.googleapis.com; font-src 'self'; object-src 'none'; media-src 'none'; frame-src 'none'; report-uri http://localhost:8080/csp-report; form-action 'self'; frame-ancestors 'none'; base-uri 'self'; manifest-src 'self'; script-src-attr 'none'; upgrade-insecure-requests",
+        "default-src 'none'; script-src 'self' 'unsafe-inline'; style-src 'unsafe-inline'; img-src 'self' https: data:; connect-src 'self' http://localhost:8080 https://storage.googleapis.com; font-src 'self'; object-src 'none'; media-src 'self' blob:; frame-src 'none'; report-uri http://localhost:8080/csp-report; form-action 'self'; frame-ancestors 'none'; base-uri 'self'; manifest-src 'self'; script-src-attr 'none'; upgrade-insecure-requests",
     },
   },
   publicDir: "static",
